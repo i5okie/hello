916502602361793456a97062f77c39b65fe84777
Ivan P <ipzent@gmail.com>
2018-07-03 11:59:06 -0700

    initial

---
+++ scripts/compile.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
+
+### Update these values for your own S3 bucket ###
+SECRETS_S3_BUCKET=phoenix-app-secrets-store
+S3_BUCKET_REGION=us-west-2
+### Don't update below this line ###
+
+VERSION=$(grep version mix.exs | sed 's/^.*version: "//' | sed 's/",//')
+APP_NAME=$(grep app: mix.exs | sed 's/^.*app: ://' | sed 's/,//')
+
+# Load the S3 secrets file contents into the environment variables
+aws s3 cp s3://$SECRETS_S3_BUCKET/creds.txt --region $S3_BUCKET_REGION creds.txt
+eval $(cat creds.txt | sed 's/^/export /')
+rm creds.txt
+
+# Install deps
+mix local.hex --force
+mix local.rebar --force
+mix deps.get
+npm install
+
+# Build assets and release
+./node_modules/brunch/bin/brunch build --production
+MIX_ENV=prod mix phoenix.digest
+MIX_ENV=prod mix release --env=prod
+echo $?
+
+# Move release into run location, allow execute by owner
+mkdir -p /var/app/current
+chown owner /var/app/current
+cp _build/prod/rel/$APP_NAME/releases/$VERSION/$APP_NAME.tar.gz /var/app/current/$APP_NAME.tar.gz
+cd /var/app/current
+tar -xzf $APP_NAME.tar.gz
+chown -R owner ./*
